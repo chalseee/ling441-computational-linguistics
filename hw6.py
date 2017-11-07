bc0ea9dd0c9b6c4501b26b02171694058211e8b1
chalseoko <chalseoko@gmail.com>
2017-11-07 15:22:38 -0800

    problem 3 done

--- hw6.py
+++ hw6.py
@@ -39,13 +39,29 @@ class Tagger (object):
 					
 #3
 	def edge_score(self, prv, nxt):
-		#up to prev node
+		#up to prev node - previous path score + emission cost
 		result = prv.score + self.model.ecost(prv.pos, prv.word)
-		
-		#find last/next node
-		for nd in nxt.prev_nodes:
-			print(nd)
-		return result
+
+		#find transmission cost
+		next_score = self.model.tcost(prv.pos, nxt.pos)
+		return result + next_score
+
+	def score_node(self, node):
+		tmp = 10000
+		for nd in node.prev_nodes:
+			print(nd.score)
+			if (tmp > self.edge_score(nd, node)):
+				tmp = self.edge_score(nd, node)
+				node.best_prev = nd
+		return tmp
+
+	def score_graph(self):
+		self.nodes[0].score = 0
+		valid_nodes = self.nodes[1:]
+
+		for nd in valid_nodes:
+			nd.score = self.score_node(nd)
+
 
 #Tests
 print("#1 tests: ")
@@ -64,6 +80,9 @@ n1.score = .1
 n2.score = .9
 print('edge_score(n1,n3) =',tagger.edge_score(n1, n3))
 print('edge_score(n2,n3) =', tagger.edge_score(n2,n3))
-#print('\nscore_node(n3) =', tagger.score_node(n3))
-#print('n3.best_prev =', n3.best_prev)
+print('\nscore_node(n3) =', tagger.score_node(n3))
+print('n3.best_prev =', n3.best_prev)
+tagger.score_graph()
+print('tagger.score_graph():')
+print_graph(tagger.nodes)
  
