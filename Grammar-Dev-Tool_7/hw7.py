6ff8e0d8d59baf2bfee3efe894e934f7db6b52d5
chalseoko <chalseoko@gmail.com>
2017-11-23 15:23:19 -0500

    finished #1-8

--- Grammar-Dev-Tool_7/hw7.py
+++ Grammar-Dev-Tool_7/hw7.py
@@ -1,3 +1,60 @@
 #Chalse Okorom-Achuonye
 #LING 441 - Homework 7
+from nltk import CFG, ChartParser, tokenize, tree
 
+class GDev (object):
+    def __init__(self, n):
+        self.name = n
+
+    def load_grammar(self):
+        file = open(str(self.name + '.cfg')).read()
+        self.grammar = CFG.fromstring(file)
+        
+    def reload(self):
+        self.load_grammar()
+        self.parser = ChartParser(self.grammar)  
+
+    def load_sents(self):
+        #using a textfile instead of a .sents? won't let me make a .sents file?
+        file = open(str(self.name + '.sents.txt'))
+        self.sents = []
+        for line in file:
+            if line[0] == '*':
+                self.sents.append((False, line.strip()[1:]))
+            else:
+                self.sents.append((True, line.strip()))                                                                                                                                                                                                                                                                                                        
+    
+    def parses(self, sentence):
+        words = tokenize.word_tokenize(sentence)
+        try:
+            return list(self.parser.parse(words))[0]
+        except:
+            return None  
+        
+    def regress(self):
+        for s in self.sents: 
+            prediction = type(self.parses(s[1])) == tree.Tree
+            if prediction != s[0]:
+                if s[0] == False:
+                    print ("!!" + " " + "*" + s[1])
+                else:
+                    print ("!!" + " " + " " + s[1])
+            else:
+                if s[0] == False:
+                    print ("  " + " " + "*" + s[1])
+                else:
+                    print ("  " + " " + " " +  s[1])
+                    
+    def __call__(self):
+        self.reload()
+        self.regress()
+            
+
+gd = GDev('g1')
+gd.load_grammar()
+gd.reload()
+print(gd.parses("Mary walked the dog"))
+print(gd.parses("Gorp glimped"))
+gd.load_sents()
+gd.regress()
+gd.__call__()
